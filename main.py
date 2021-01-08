2bba7709c7678cbda82e229cab690c2d8cf799a0
qanpi <qanpii@gmail.com>
2021-01-08 16:33:47 +0200

    added new graphs and refactored old code

--- main.py
+++ main.py
@@ -4,29 +4,28 @@ import re
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 from collections import Counter
+from datetime import datetime
 
+#PARSING --------------------------------------------------------------------------------------------
 
-#Opening the file and initialzing the bs4 parser ----------------------------------------------------------------
-with open("test2.html") as html_doc:
+#Opening the file and initialzing the bs4 parser 
+with open("test_full.html") as html_doc:
     soup = BeautifulSoup(html_doc, "html.parser")
-
-#MOVING LATER
-
 
 #IB [0,8] to finnish grade [4,10] using linear mapping
 def to_finnish_grade(x, denom):
     grade = x/denom*6+4
     return grade - (grade%0.25)
 
-#Parsing the data and putting it into a dictionary --------------------------------------------------------------
+#Parsing the data and putting it into a dictionary 
 rows = soup.find("tbody").find_all("tr")
-parsed_data = np.empty(len(rows), list)
+parsed_data = np.empty((5, len(rows)), "U50")
 
 class Cell:
-    types = ["Date", "Teacher", "Subject", "Info", "Grade", "IB"]
-
+    #Exceptions and notations
     grade_exceptions = {
         "91/2": "9½"
     }
@@ -38,17 +37,18 @@ class Cell:
     "" : 0
     } 
 
-    def __init__(self, j, s):
-        self.type = Cell.types[j]
+    def __init__(self, t, s):
+        self.type = t
         self.string = s
 
     def remove_whitespaces(self):
         self.string = re.sub(r"\s+", " ", self.string)
         
     def parse(self):
+        output = ""
         if self.type == "Subject":
             match = re.search(r": *([\w+, ]*$)", self.string)
-            output = match.group(1) if match else None
+            if match: output = match.group(1)
 
         elif self.type == "Grade":
             if self.string in Cell.grade_exceptions: self.string = Cell.grade_exceptions[self.string] #check for predefined edge cases
@@ -62,43 +62,84 @@ class Cell:
             elif match := re.search(r"(\d+)(\D*)", self.string): #format 3, e.g. 9+
                 grade, mod = match.groups()
                 output = int(grade) + Cell.grade_modifiers[mod]
-            else: output = None
         
         else: output = self.string
-        return output 
+        return str(output) 
+
+#The "engine" which pushes the data to parse
+types = ["Date", "Teacher", "Subject", "Grade", "IB"]
 
 for i, row in enumerate(rows):
     cols = row.find_all("td")
-    col = []
+    cols.pop(3) #remove the additional information column as it is useless and impractical
 
     for j, cell in enumerate(cols):
-        cell = Cell(j, cell.get_text(strip=True))
+        cell = Cell(types[j], cell.get_text(strip=True))
         cell.remove_whitespaces()
-        col.append(cell.parse())
+        parsed_data[j,i] = cell.parse()
 
-    parsed_data[i] = col
         
-#Histogram of grades
-grades   = [value[4] for value in parsed_data]
-subjects = [value[2] for value in parsed_data]
+#CHARTS AND DATA --------------------------------------------------------------------------------------------
 
-data = list(zip(subjects, grades))
-data = list(filter(lambda x: x[1] != None, data))
+#Setup
+fig, axes = plt.subplots(2,2)
 
-count = dict(Counter([v[1] for v in data]))
-print(count.keys(), count.values())
+#Extract data
 
-print("Mean: ", np.mean([v[1] for v in data]))
+dates, teachers, subjects, grades, ibs = np.split(parsed_data.flatten(), 5)
 
-plt.pie(count.values(), labels=count.keys())
+#CHART 0 ----------------------------------------------
+
+#Cleaning up/purifying
+grades_clean = grades[grades != ""].astype(np.float)
+grades_clean = grades_clean[grades_clean > 0]
+
+unique, counts = np.unique(grades_clean, return_counts=True)
+print(unique, counts)
+#Graphing
+axes[0,1].bar(unique, counts, width=0.25, align="center")
+axes[0,1].axvline(grades_clean.mean(), color="firebrick", linestyle="dashed", linewidth=1.5)
+
+
+
+#CHART 1 ----------------------------------------------
+mask = (dates != "") & (grades != "")
+
+dates_parsed = [datetime.strptime(s, "%a %d.%m.%Y") for s in dates[mask][::-1]]
+grades_parsed = grades[mask].astype(np.float)
+
+months = [d.month for d in dates_parsed]
+
+split_indeces = np.where(np.abs(np.diff(months))>1)[0] + 1
+print(np.diff(months))
+
+data10 = np.split(dates_parsed, split_indeces)
+data11 = np.split(grades_parsed, split_indeces)
+
+full_range = sorted(set(grades_parsed))
+test2 = np.empty(0)
+print(full_range)
+terms = ["Autumn", "Spring"]
+
+for i in range(len(data11)):
+    data10[i] = terms[data10[i][0].month // 7] + " " + str(data10[i][0].year)
+    data11[i] = dict(Counter(data11[i]))
+    
+    test = [data11[i][g] if g in data11[i] else 0 for g in full_range]
+    test2 = np.append(test2, test)
+
+
+test2 = np.reshape(test2, (-1, len(full_range)))
+test2_cum = test2.cumsum(axis=1)
+
+# data10 = [d.strftime("%b %y") for d in data10]
+# data10 = [label if i%2==0 else "\n" + label for i,label in enumerate(data10)]
+
+for i in range(8):
+    heights = test2[:, i]
+    starts = test2_cum[:, i] - heights
+    axes[0,0].bar(data10, heights, bottom=starts, width=0.5)
+
 plt.show()
-
-# plt.bar()
-# plt.show()
-
-
-
-# plt.hist(grades, 3)
-# plt.show()
 
 
